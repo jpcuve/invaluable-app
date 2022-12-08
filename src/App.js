e7336f704a41baa0bcda083f32d286b94fd4ed11
Jean-Pierre Cuvelliez <jpcuvelliez@gmail.com>
2022-12-08 12:01:08 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import algoliasearch from 'algoliasearch';
 
 function App() {
+    const [term, setTerm] = useState('')
+    const [index, setIndex] = useState()
+    const [results, setResults] = useState([])
+
+    const handleSubmit = async e => {
+        e.preventDefault()
+        if (index){
+            console.log(`Searching for term: ${term}`)
+            const res = await index.search(term)
+            console.log(res)
+            res.hits.forEach(it => {
+                console.log(JSON.stringify(it))
+            })
+            setResults(res.hits)
+        }
+    }
+    useEffect(() => {
+        const client = algoliasearch('0HJBNDV358', 'NO_KEY', {hosts: [
+            {url: 'algolia.invaluable.com'}
+        ]})
+        console.log(`Client: ${JSON.stringify(client)}`)
+        const index = client.initIndex('upcoming_lots_prod')
+        setIndex(index)
+        console.log(`Index: ${JSON.stringify(index)}`)
+        return () => {
+            if (client){
+                // client.destroy()
+            }
+        }
+    }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+          <input type="text" value={term} onChange={e => setTerm(e.target.value)} placeholder="Search"/>
+      </form>
+        {results.map(it => (
+            <div key={it.lotNumber}>
+                <pre>{JSON.stringify(it, null, 2)}</pre>
+                <hr/>
+            </div>
+        ))}
     </div>
   );
 }
